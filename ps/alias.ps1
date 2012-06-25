c19d59d94c174b86e5b3dec10fc881438c961eed
jittuu <soemoetunlwin@gmail.com>
2012-06-25 11:11:07 +0800

    added alias

---
+++ ps/alias.ps1
@@ -0,0 +1,27 @@
+# git
+function Git-Status { git status -s @args }
+Set-Alias g_s Git-Status
+
+function Git-Pull { git pull --rebase @args }
+Set-Alias g_l Git-Pull
+
+function Git-Push { git push @args }
+Set-Alias g_p Git-Push
+
+function Git-AddAll { git add -A }
+Set-Alias g_a Git-AddAll
+
+function Git-Commit { git commit @args }
+Set-Alias g_c Git-Commit
+
+function Git-CheckOut { git checkout @args }
+Set-Alias g_co Git-CheckOut
+
+function Git-Diff { git diff @args }
+Set-Alias g_d Git-Diff
+
+# nuget
+Set-Alias n_ls Get-Package
+Set-Alias n_i Install-Package
+Set-Alias n_rm Uninstall-Package
+Set-Alias n_u Update-Package
--- ps/profile.ps1
+++ ps/profile.ps1
@@ -4,10 +4,10 @@ function prompt() {
     $dir = [System.IO.Path]::GetFileName($loc)
     Write-Host "PS /" -nonewline
     Write-Host "$($dir)" -nonewline -Foreground Yellow
-      
+
     # writing git status
     $status = Get-GitStatus
-    
+
     if($status) {
         $currentBranch = $status.Branch
         $branchColor = [ConsoleColor]::Green
@@ -25,7 +25,7 @@ function prompt() {
         }
         Write-Host -NoNewline "]"
     }
-    
+
     return " > "
 }
 
@@ -33,19 +33,22 @@ function prompt() {
     # import modules
     Import-Module PsGet
     Import-Module pscx
-    Import-Module PsUrl   
+    Import-Module PsUrl
 
     # If Posh-Git environment is defined, load it. which is installed by GitHub For Windows
     if (test-path env:posh_git) {
         . $env:posh_git
-    } 
+    }
     else {
         Import-Module Posh-Git
-        
+
         # set TERM as msys
         $env:TERM = "msys"
-        
+
         # start ssh
         Start-SshAgent -Quiet
     }
 }
+
+# alias
+. (Resolve-Path ~/Documents/WindowsPowershell/alias.ps1)
