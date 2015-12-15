57572ecf8428b4616f86590d3e04faa555df2c33
Soe Moe <soemoetunlwin@gmail.com>
2015-12-15 22:24:51 +0630

    remove vs variables

--- ps/profile.ps1
+++ ps/profile.ps1
@@ -29,19 +29,6 @@ function prompt() {
     return " > "
 }
 
-function VsCmd() {
-    #Set environment variables for Visual Studio Command Prompt
-    pushd 'c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC'
-    cmd /c "vcvarsall.bat&set" |
-    foreach {
-      if ($_ -match "=") {
-        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-      }
-    }
-    popd
-    write-host "`nVisual Studio 2010 Command Prompt variables set." -ForegroundColor Green
-}
-
 function Delete-MergedBranches ($Commit = 'HEAD', [switch]$Force) {
     git branch --merged $Commit |
         ? { $_ -notmatch '(^\*)|(^. master$)' } |
@@ -67,6 +54,3 @@ function Delete-MergedBranches ($Commit = 'HEAD', [switch]$Force) {
 
 # alias
 . (Resolve-Path ~/Documents/WindowsPowershell/alias.ps1)
-
-# VsCmd
-VsCmd
